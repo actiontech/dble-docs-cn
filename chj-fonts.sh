2430407742d06368d3e54de781d924e53fe312f8
yanhuqing666 <yanhuqing666@users.noreply.github.com>
2024-09-20 15:40:40 +0800

    Updated by gitbook-action 2024-09-20 15:38:14

--- chj-fonts.sh
+++ chj-fonts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! -d /root/.fonts" ] ; then
+if [ ! -d "/root/.fonts" ] ; then
     mkdir /root/.fonts
     echo -e "create ~/.fonts/ \n"
 fi
@@ -7,13 +7,13 @@ fi
 # Download
 
 wget -O SourceHanSansHWSC.zip -nv https://github.com/adobe-fonts/source-han-sans/releases/download/2.004R/SourceHanSansHWSC.zip
-echo -e "download finished"
+echo -e "download finished\n"
 
 unzip SourceHanSansHWSC.zip
 
 # Copy fonts to font directory
-cp -i OTF/SimplifiedChineseHW/SourceHanSansHWSC-Regular.otf /root/.fonts
-cp -i OTF/SimplifiedChineseHW/SourceHanSansHWSC-Bold.otf /root/.fonts
+cp -f OTF/SimplifiedChineseHW/SourceHanSansHWSC-Regular.otf /root/.fonts
+cp -f OTF/SimplifiedChineseHW/SourceHanSansHWSC-Bold.otf /root/.fonts
 
 sudo fc-cache -fv
 
